61756c0a6674daea7848b0d404227632538a0e35
tomioka-k <62204434+tomioka-k@users.noreply.github.com>
2021-08-14 20:09:14 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,14 @@
+import { useLocation, useHistory } from "react-router-dom";
+
+export const Page1DetailA = () => {
+  const { state } = useLocation();
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
+
+  return (
+    <div>
+      <h1>Page1DetalAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
